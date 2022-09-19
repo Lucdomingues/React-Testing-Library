6fcc014c07db0a3c6e9df46e96c8c48e2b65a17f
Lucdomingues <lojalucc@outlook.com>
2022-09-19 18:39:48 -0300

    requisito 02

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../pages/About';
+
+describe('Verifica o componente `About`', () => {
+  test('Verifica se existe informações da Pokedéx', () => {
+    renderWithRouter(<About />);
+
+    const textInformation = screen.getAllByText(/pokédex/i);
+    expect(textInformation[1]).toBeInTheDocument();
+  });
+
+  test('Verifica se existe um texto `About Pokédex`', () => {
+    renderWithRouter(<About />);
+
+    const titleAbout = screen
+      .getByRole('heading', { name: /About Pokédex/i, level: 2 });
+    expect(titleAbout).toBeInTheDocument();
+  });
+
+  test('Verifica se existem dois parágrafos sobre a Pokédex`', () => {
+    renderWithRouter(<About />);
+
+    const paragrafoAbout = screen
+      .getAllByText(/Pokémons/i);
+    expect(paragrafoAbout[0]).toBeInTheDocument();
+    expect(paragrafoAbout[1]).toBeInTheDocument();
+  });
+
+  test('Verifica se existe uma img de uma Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const imgAbout = screen.getByAltText(/pokédex/i);
+
+    expect(imgAbout).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
