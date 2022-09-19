77828ac82fbe62f6ede56bdb4e34a404b4a6ecfc
Lucdomingues <lojalucc@outlook.com>
2022-09-19 20:12:08 -0300

    requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Verifica o componente FavoritePokemons', () => {
+  test('Verifica se é exebido a menssagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const textFound = screen.getByText('No favorite pokemon found');
+    expect(textFound).toBeVisible();
+  });
+});
