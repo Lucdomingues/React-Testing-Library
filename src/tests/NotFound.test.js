9d31adf4411d7e3252316521e2a7c9a4f7baebf2
Lucdomingues <lojalucc@outlook.com>
2022-09-19 20:25:25 -0300

    requisito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Verifica o componente NotFound', () => {
+  test('Verifica se tem um h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const titleNotFound = screen
+      .getByRole('heading', { name: /Page requested not found/i, level: 2 });
+    expect(titleNotFound).toBeInTheDocument();
+  });
+
+  test('Verifica se existe uma img com a `src` correta', () => {
+    renderWithRouter(<NotFound />);
+
+    const imgNotFound = screen
+      .getByAltText(/Pikachu crying because the page requested was not found/i);
+
+    expect(imgNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
